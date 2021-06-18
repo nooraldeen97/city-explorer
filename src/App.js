66990be349d5a8d1bbd40da17ed47218f068e541
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-06-18 15:04:41 +0300

    rendered data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Form,Button,Card} from 'react-bootstrap/';
+import {Form,Button,Card,CardColumns,Table} from 'react-bootstrap/';
 import axios from 'axios';
-
+import Movie from './components/Movie';
+import Weather from './components/Weather';
 
 class App extends React.Component{
 
@@ -11,22 +12,39 @@ class App extends React.Component{
     this.state={
       dataRenderedInCard:'',
       errorMsg:'',
-      renderedLocWeatherData:'',
+      renderedLocWeatherData:[],
       displayMap:false,
       displarErrorMsg: false,
       showWeather : false,
       weatherErr:'',
       dspErrWeather:false,
+      movieInfo:'',
+      showMovie:false,
+      movieError:''
     }
   }
-
 
   showData= async (event)=>{
     event.preventDefault();
     let inputCity= event.target.userInput.value;
     let locURL = `https://us1.locationiq.com/v1/search.php?key=pk.370701c55ed7503519f7418b1098f8d2&q=${inputCity}&format=json`;
     let weatherData = `${process.env.REACT_APP_URL}/weather?searchQuery=${inputCity}`;
-
+    let movieData= `${process.env.REACT_APP_URL}/movies?city=${inputCity}`;
+    try{
+    let movieResult= await axios.get(movieData);
+    console.log(movieResult.data);
+      this.setState({
+        movieInfo:movieResult.data,
+        showMovie:true
+      })
+      console.log(this.state.movieInfo)
+    }
+    catch(err){
+      this.setState({
+        movieError:err,
+        showMovie:false
+      })
+    }
     try{
       let locResult = await axios.get(locURL);
       this.setState({
@@ -49,18 +67,29 @@ class App extends React.Component{
 
     try{
       let locWeatherData = await axios.get(weatherData);
-       this.setState({
-         renderedLocWeatherData : locWeatherData.data[0],
-         showWeather : true , 
-         dspErrWeather:false,
-       })
-     
+      console.log(locWeatherData.data)
+      if(locWeatherData.data !== undefined){
+         this.setState({
+           renderedLocWeatherData : locWeatherData.data,
+           showWeather : true , 
+           dspErrWeather:false,
+         })
+         
+       }else{
+        this.setState({
+          weatherErr:'sorry , no weather data availabe for your location',
+          showWeather : false ,
+          dspErrWeather:true
+  
+        })
+       }
     }
     catch{
       this.setState({
         weatherErr:'sorry , no weather data availabe for your location',
         showWeather : false ,
         dspErrWeather:true
+
       })
 
     }
@@ -91,12 +120,39 @@ class App extends React.Component{
           </Card.ImgOverlay>
         </Card>}
           {this.state.displarErrorMsg && <p>{ this.state.errorMsg}</p>}  
-    
-      {this.state.showWeather && <Card>
-          <Card.Body>    
-                <p>{this.state.renderedLocWeatherData.date}</p>
-          </Card.Body>
-        </Card>}
+
+                {this.state.showWeather &&
+       <Table striped bordered hover>
+            <thead>
+                <tr>
+                  <th>date</th>
+                  <th>description</th>
+                </tr>
+            </thead>
+           {this.state.renderedLocWeatherData.map((element)=>{
+             return(<Weather
+              description={element.description}
+              date={element.date}
+             />
+             )
+           })}
+      </Table>
+           }
+        <CardColumns>
+        {this.state.showMovie &&
+         this.state.movieInfo.map(item=>(
+          <Movie
+          Title={item.title}
+          description={item.overview}
+          imgURL={item.image_ur}
+          date={item.released_on}
+          vote={item.average_votes}
+          />
+        ))}
+        </CardColumns>
+          {this.state.movieError && 
+        <p>error in getting data</p>}
+
         {/* {this.state.dspErrWeather && <p>{this.state.weatherErr}</p>}  */}
       </div>
     )
